9f9bb9c2a5a5ca41985b94949d95f44a452a38f0
hsokey-berk <hsokey@berkeley.edu>
2026-09-23 23:31:42 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,9 @@
+// config.js
 var defined_csv = "Sept2026.csv";
+
+// Default site and cable length
+var default_site = "Rivendell";
+var default_cable = 10;
+
+// Wells to select by default for Rivendell
+var default_wells_rivendell = ["16","10","15","7","6","5","14","1","12","3","2"];
